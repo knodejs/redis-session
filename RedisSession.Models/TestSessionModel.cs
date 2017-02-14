155d7c9c96142282aa5ac898219056b1ffbd9f2c
Narongchai Thongkongkeaw <nat@freshket.co>
2017-02-14 17:10:12 +0700

    Add model for shared between web and api

---
+++ RedisSession.Models/TestSessionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisSession.Models
+{
+    public class TestSessionModel
+    {
+        public int Id { get; set; }
+        public string TestName { get; set; }    
+    }
+}
